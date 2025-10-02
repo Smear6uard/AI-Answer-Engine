45aa8a83df6ed942f4b3383f1a4c8722e97fc967
Sameer Akhtar <sakhtar9@depaul.edu>
2025-10-02 17:35:49 -0500

    Initial commit - AI Answer Engine in progress

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -3,13 +3,40 @@
 // Refer to the Groq SDK here on how to use an LLM: https://www.npmjs.com/package/groq-sdk
 // Refer to the Cheerio docs here on how to parse HTML: https://cheerio.js.org/docs/basics/loading
 // Refer to Puppeteer docs here: https://pptr.dev/guides/what-is-puppeteer
+import {NextResponse} from "next/server"
+import { getGroqResponse } from "@/app/utils/groqClient"  
+import {scrapeURL, urlPattern} from "@/app/utils/scrapers"
 
 export async function POST(req: Request) {
   try {
+    const {message} = await req.json()
 
+    const url = message.match(urlPattern);
+
+    let scrapedContent="";
+if (url){
+  console.log('url found', url)
+  const scraperResponse = await scrapeURL(url);
+  console.log('scraped content', scrapedContent);
+  scrapedContent = scraperResponse.content;
+}
+   const userQuery = message.replace(url ? url[0] : '', '').trim();
+
+   const prompt = `
+   Answer my question: "${userQuery}"
+
+   Based on the following context:
+   <content>
+    ${scrapedContent}
+   </content>
+   `
+    console.log('Prompt', prompt)
+    const response = await getGroqResponse(prompt)
+
+    return NextResponse.json ({message: response})
 
   } catch (error) {
 
-
+return NextResponse.json({message: "error"})
   }
 }
